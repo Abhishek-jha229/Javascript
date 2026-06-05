ff63db2e85beccc48ce005b914328ccddce24520
Abhishek-jha229 <jha83583@gmail.com>
2026-06-05 19:25:22 +0530

    Created 01_Variables.js

---
+++ 01_Js_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+// var, let  ,const
+
+//var------>> it is an outdated method
+var x=29;
+x=229
+console.log(x);
+
+//let ----->>it is commonly used
+let y=229;
+y=200;
+console.log(y);
+
+//const ----->> it is used for constant terms which cannot be changed after declaring
+const z=15;
+// z=16;     not allowed
+console.log(z);
