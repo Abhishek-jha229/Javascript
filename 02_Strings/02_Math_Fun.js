f98dcf8b281c9d84d3a4e1c49f9b6c7e62d1a54a
Abhishek-jha229 <jha83583@gmail.com>
2026-06-05 19:34:37 +0530

    Created 02_Math_Fun.js

---
+++ 02_Strings/02_Math_Fun.js
@@ -0,0 +1,22 @@
+// let num=229.6945
+// console.log(typeof(num.toString()));
+// console.log(num.toFixed(2));
+// console.log(num.toExponential(2));
+// console.log(num.toPrecision(3));
+// console.log(num.valueOf());
+
+
+
+//math
+
+//floor and ceil
+
+console.log(Math.E);
+
+//printing random numbers
+
+//Math.floor(Math.random()*(max-min+1))+min;
+
+let min=0;
+let max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
